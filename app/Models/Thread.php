fc6ecec7e9b441a6a65187078c17385dc09d56c8
Fadhil Muhammad <fadhilmhmd29@gmail.com>
2021-08-01 22:49:18 +0700

    Category and Thread Models setup /w its migration.

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Thread extends Model
 {
-    use HasFactory;
+  use HasFactory;
+
+  public function category(): BelongsTo
+  {
+    return $this->belongsTo(Category::class);
+  }
 }
